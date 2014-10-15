6aafe13f40f0e8233f7be383b16df0fd30f89710
Robert Cooke <robert.cooke@trimm.nl>
2014-10-15 18:43:15 +0200

    cc

---
+++ src/impl/java/io/core9/client/FileWidget.java
@@ -0,0 +1,8 @@
+package io.core9.client;
+
+import io.core9.core.plugin.Core9Plugin;
+import io.core9.plugin.server.handler.Middleware;
+
+public interface FileWidget extends Core9Plugin, Middleware {
+
+}
---
+++ src/impl/java/io/core9/client/FileWidgetImpl.java
@@ -0,0 +1,17 @@
+package io.core9.client;
+
+import io.core9.plugin.server.request.Request;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@PluginImplementation
+public class FileWidgetImpl implements FileWidget {
+
+
+
+	@Override
+	public void handle(Request request) {
+		// TODO Auto-generated method stub
+		System.out.println("");
+	}
+
+}
