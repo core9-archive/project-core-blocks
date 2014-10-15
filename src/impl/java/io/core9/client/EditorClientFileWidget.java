87a2902e741d9c9393a6cd7c0c31ff17871d1e19
Robert Cooke <robert.cooke@trimm.nl>
2014-10-15 18:14:43 +0200

    cc

--- src/impl/java/io/core9/client/EditorClientFileWidget.java
+++
@@ -1,8 +0,0 @@
-package io.core9.client;
-
-import io.core9.core.plugin.Core9Plugin;
-import io.core9.plugin.widgets.widget.Widget;
-
-public interface EditorClientFileWidget extends Widget, Core9Plugin {
-
-}
