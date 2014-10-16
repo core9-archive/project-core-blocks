8d61f91d73037cfdd53a932750e84f5c1593688e
jessec <robertjessecooke@gmail.com>
2014-10-16 17:28:33 +0200

    cc

--- src/impl/java/io/core9/client/FileDataHandlerImpl.java
+++ src/impl/java/io/core9/client/FileDataHandlerImpl.java
@@ -2,14 +2,23 @@ package io.core9.client;
 
 import io.core9.editor.AssetsManager;
 import io.core9.editor.AssetsManagerImpl;
+import io.core9.editor.ClientRepositoryImpl;
 import io.core9.editor.FileDataHandler;
+import io.core9.editor.RequestImpl;
 import io.core9.plugin.admin.plugins.AdminConfigRepository;
 import io.core9.plugin.server.request.Request;
 import io.core9.plugin.widgets.datahandler.DataHandler;
 import io.core9.plugin.widgets.datahandler.DataHandlerFactoryConfig;
+import io.undertow.util.Headers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import net.xeoh.plugins.base.annotations.injections.InjectPlugin;
@@ -36,45 +45,62 @@ public class FileDataHandlerImpl implements FileDataHandler<FileDataHandlerConfi
 	public DataHandler<FileDataHandlerConfig> createDataHandler(final DataHandlerFactoryConfig options) {
 		return new DataHandler<FileDataHandlerConfig>() {
 
+			private ClientRepositoryImpl clientRepository;
+
 			@Override
 			public Map<String, Object> handle(Request req) {
 
+				AssetsManager assetsManager = new AssetsManagerImpl(pathPrefix);
+				clientRepository = new ClientRepositoryImpl();
+				clientRepository.addDomain("www.easydrain.nl", "easydrain");
+				clientRepository.addDomain("localhost", "easydrain");
+				RequestImpl request = new RequestImpl();
+				request.setClientRepository(clientRepository);
+				String absoluteUrl = "http://" + req.getHostname() + req.getPath();
+				request.setAbsoluteUrl(absoluteUrl);
+				assetsManager.setRequest(request);
 
-				AssetsManager assetManager = new AssetsManagerImpl(pathPrefix);
-
-
-				Map<String,Object> result = new HashMap<String,Object>();
+				Map<String, Object> result = new HashMap<String, Object>();
 				Map<String, Object> menu = configRepository.readConfig(req.getVirtualHost(), ((FileDataHandlerConfig) options).getClientId(req));
-				if(menu == null) {
-					menu = new HashMap<String,Object>();
-				}
-				result.put("file", "file");
-
-				String request = req.getPath();
-
-
-				String filename = "/site/assets/css/core.css";
-
-				String file = assetManager.getStaticFilePath(filename);
-
-
-				byte[] data = fileToBinary(file);
-
-				req.getResponse().sendBinary(data);
-
-				if(request.startsWith("/site/blocks/")){
-
-				}else{
-
+				if (menu == null) {
+					menu = new HashMap<String, Object>();
 				}
 
+				String path = req.getPath();
+
+				String file = assetsManager.getStaticFilePath(path);
+
+				// byte[] data = fileToBinary(file);
+				String data = readFileFromPath(file);
+
+				req.getResponse().putHeader("Content-Type", "text/css");
+				//req.getResponse().end(data);
+
+				result.put("file", data);
 
 				return result;
 			}
 
-			private byte[] fileToBinary(String filename) {
-				// TODO Auto-generated method stub
+			public String readFile(String path, Charset encoding) throws IOException {
+				byte[] encoded = Files.readAllBytes(Paths.get(path));
+				return new String(encoded, encoding);
+			}
+
+			private String readFileFromPath(String file) {
+				try {
+					return readFile(file, StandardCharsets.UTF_8);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				return null;
+			}
+
+			@SuppressWarnings("unused")
+			private byte[] fileToBinary(String filename) {
+				File file = new File(filename);
+				byte[] fileData = new byte[(int) file.length()];
+				return fileData;
 			}
 
 			@Override
